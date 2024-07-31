99849086926c510a707785d3d09db3a44a75645b
David-0102 <davidmumondunda@gmail.com>
2024-07-31 07:51:25 -0700

    0-hello_route.py

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""
+This is the __init__.py file for the web_flask module.
+"""
+# This file can be empty or contain initialization code for the package.
+#!/usr/bin/python3
+"""
+This is the __init__.py file for the web_flask module.
+"""
+# This file can be empty or contain initialization code for the package.
 
